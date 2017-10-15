830628bee9a6690d42c4dcd534c17e24598b1f7c
chindit <littletiger58@gmail.com>
2017-10-15 19:51:05 +0200

    First commit

---
+++ src/Entity/User.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface, EquatableInterface
+{
+    /** @var string  */
+    private $username;
+    /** @var string  */
+    private $password;
+    /** @var string  */
+    private $salt;
+    /** @var array  */
+    private $roles;
+
+    public function __construct(string $username, string $password, string $salt, array $roles)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->salt = $salt;
+        $this->roles = $roles;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getSalt(): string
+    {
+        return $this->salt;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function isEqualTo(UserInterface $user): bool
+    {
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if ($this->password !== $user->getPassword()) {
+            return false;
+        }
+
+        if ($this->salt !== $user->getSalt()) {
+            return false;
+        }
+
+        if ($this->username !== $user->getUsername()) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/Security/UserProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserProvider
+{
+    public function loadUserByUsername($username): User
+    {
+        $userData = new User('', '', '', []);
+
+        if ($userData) {
+            return $userData;
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user): User
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class): bool
+    {
+        return User::class === $class;
+    }
+}
