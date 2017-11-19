898531943110583e73b621e4ed273669e28edf63
chindit <littletiger58@gmail.com>
2017-11-19 19:49:25 +0100

    Update vendors

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
